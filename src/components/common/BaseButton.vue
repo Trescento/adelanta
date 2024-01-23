1468fbf7b28607582bbbe7e5ded3e96653265db5
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-23 17:15:22 -0600

    Add BaseButton component

---
+++ src/components/common/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :class="buttonClass">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  computed: {
+    buttonClass() {
+      return {
+        'text-lg text-white bg-brand font-semibold px-6 py-4 rounded-lg mt-[40px] flex items-center space-x-4': this.type === 'primary',
+        'text-lg text-brand font-semibold px-6 py-4 rounded-lg mt-[40px] flex items-center space-x-4': this.type === 'secondary'
+      }
+    }
+  }
+}
+</script>
