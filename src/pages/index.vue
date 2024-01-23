30a05af682254eba3a247f045c3ddb5b6252aae7
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-23 17:15:52 -0600

    Init new section on index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,23 @@
 <template>
   <HeaderApp />
-  <section class="container flex flex-col items-center my-24">
-    <h1 class="font-bold text-6xl text-center max-w-5xl leading-[normal]">
-      <span class="text-brand">Accede capital para crecer</span> tu negocio sin endeudarte
-    </h1>
-    <button class="text-lg text-white bg-brand font-semibold px-6 py-4 rounded-lg mt-[40px] flex items-center space-x-4">
-      <span>Solicita capital por adelantado</span>
-      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-        <path d="M5.5 12H19.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M12.5 5L19.5 12L12.5 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    </button>
+  <HeroSection />
+  <section class="container">
+    <div class="flex justify-between">
+      <h2 class="font-bold text-4xl"><span class="text-brand">¿Cómo funciona</span> Adelanta por Ziff?</h2>
+      <BaseButton type="primary">
+        Obtén una cotización de inmediato
+      </BaseButton>
+    </div>
   </section>
 </template>
+
+<script>
+import BaseButton from '~/components/common/BaseButton.vue';
+
+export default {
+  components: {
+    BaseButton
+  }
+}
+
+</script>
