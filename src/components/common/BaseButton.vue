00e2cbbc6e72f4140fd434a77bb6aac15eb51103
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-23 18:03:32 -0600

    Update BaseButton styling for secondary type

--- src/components/common/BaseButton.vue
+++ src/components/common/BaseButton.vue
@@ -16,7 +16,7 @@ export default {
     buttonClass() {
       return {
         'text-lg text-white bg-brand font-semibold px-6 py-4 rounded-lg mt-[40px] flex items-center space-x-4': this.type === 'primary',
-        'text-lg text-brand font-semibold px-6 py-4 rounded-lg mt-[40px] flex items-center space-x-4': this.type === 'secondary'
+        'text-lg border border-brand text-brand font-semibold px-6 py-4 rounded-lg mt-[40px] flex items-center space-x-4': this.type === 'secondary'
       }
     }
   }
