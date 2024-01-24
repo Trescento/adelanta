2581688e3a80747c95cebdb3db69222f7d73302d
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-24 08:48:22 -0600

    Refactor HeroSection.vue component and add BaseButton component

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="container flex flex-col items-center my-24">
-    <h1 class="font-bold text-6xl text-center max-w-5xl leading-[normal]">
+    <h1 class="font-bold text-6xl text-center max-w-5xl leading-[normal] mb-10">
       <span class="text-brand">Accede capital para crecer</span> tu negocio sin endeudarte
     </h1>
-    <button class="text-lg text-white bg-brand font-semibold px-6 py-4 rounded-lg mt-[40px] flex items-center space-x-4">
-      <span>Solicita capital por adelantado</span>
-      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-        <path d="M5.5 12H19.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M12.5 5L19.5 12L12.5 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    </button>
+    <BaseButton>
+      <div class="flex items-center space-x-4">
+        <span>Solicita capital por adelantado</span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+          <path d="M5.5 12H19.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M12.5 5L19.5 12L12.5 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </div>
+    </BaseButton>
     <div class="flex flex-nowrap overflow-hidden" ref="carouselContainer">
       <div class="flex space-x-6 text-2xl font-bold items-center mt-36"
         ref="carousel"
@@ -57,4 +59,15 @@ const words = [
   'Flujos de gestión',
   'Ingresos SaaS'
 ]
+</script>
+
+<script lang="ts">
+import BaseButton from '~/components/common/BaseButton.vue';
+
+export default {
+  components: {
+    BaseButton
+  }
+}
+
 </script>
