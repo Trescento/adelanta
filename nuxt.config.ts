e59d6a13184e4188b5a1d355f9b95e4aee9b7757
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-23 19:22:24 -0600

    Add @vueuse/motion/nuxt module and @vueuse/motion dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxtjs/google-fonts'],
+  modules: ['@nuxtjs/google-fonts', '@vueuse/motion/nuxt'],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
