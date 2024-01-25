515953cc9760e57cb7b902e122627af9db1fbd5b
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-24 22:32:00 -0600

    Update HeroSection.vue button text for responsive design

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -12,13 +12,12 @@
       </div>
       <div class="mt-6">
         <BaseButton>
-          <div class="flex items-center space-x-4">
-            <span>Solicitar capital</span>
+            <span class="block lg:hidden">Solicitar capital</span>
+            <span class="hidden lg:block">Solicita capital por adelantado</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
               <path d="M5 12.5H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M12 5.5L19 12.5L12 19.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
-          </div>
         </BaseButton>
       </div>
     </div>
