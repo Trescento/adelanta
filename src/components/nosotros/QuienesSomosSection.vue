d3c883a94059a09d8590f6b634faf9a8181bf3d5
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-27 15:53:17 -0600

    Add QuienesSomosSection component and related images

---
+++ src/components/nosotros/QuienesSomosSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="bg-brand-gray90">
+    <div class="grid grid-cols-1 md:grid-cols-2 container gap-12 py-12">
+      <div class="h-auto md:order-2">
+        <img src="/images/quienessomos.png" alt="" class="rounded-3xl h-full object-cover">
+      </div>
+      <div class="flex flex-col py-12 gap-6 md:order-1">
+        <h1 class="text-5xl font-bold">
+          ¿Quienes somos?
+        </h1>
+        <div>
+          <p class="text-xl font-bold">
+            Arrenda y Ziff se unen para brindar un producto financiero dedicado al adelanto de flujo de negocios con ingresos recurrentes, sin necesidad de garantizar o hipotecar ningún activo.
+          </p>
+          <br>
+          <p class="text-xl">
+            Somos una empresa mexicana que brinda opciones de liquidez a empresarios a través del adelanto de sus cuentas por cobrar.
+          </p>
+        </div>
+        <div class="flex items-center justify-evenly">
+          <img src="/ziff.png" alt="" class="h-14">
+          <img src="/arrenda.png" alt="" class=" h-7">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
