bdfd566f4b5d0c2d22c5869b77fb75402af049ee
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-27 00:07:13 -0600

    Update app.vue with min-height class

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="font-pjakarta text-brand-black overflow-x-hidden">
+  <div class="font-pjakarta min-h-dvh text-brand-black overflow-x-hidden">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
