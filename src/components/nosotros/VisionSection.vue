0eaf33a0e38892769d1a9921262cd0288ab55c14
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-27 16:33:21 -0600

    Add VisionSection component and vision image

---
+++ src/components/nosotros/VisionSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="bg-brand text-white">
+    <div class="container grid grid-cols-1 lg:grid-cols-2 gap-16 py-24 max-w-7xl">
+      <img src="/images/vision.png" alt="Nosotros" class="h-full w-full max-h-[587px] object-cover rounded-3xl">
+      <div class="flex flex-col gap-6">
+        <div class="bg-white flex items-center justify-center  rounded-full w-11 h-11">
+          <svg width="28" height="21" viewBox="0 0 28 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.0781 6.04688C14.2188 6.04688 14.3594 6 14.5 6C16.9844 6 19 8.01562 19 10.5C19 12.9844 16.9844 15 14.5 15C11.9688 15 10 12.9844 10 10.5C10 10.4062 10 10.2656 10 10.125C10.4219 10.3594 10.9375 10.5 11.5 10.5C13.1406 10.5 14.5 9.1875 14.5 7.5C14.5 6.98438 14.3125 6.46875 14.0781 6.04688ZM23.5 3.79688C25.7031 5.8125 27.1562 8.25 27.8594 9.9375C28 10.3125 28 10.7344 27.8594 11.1094C27.1562 12.75 25.7031 15.1875 23.5 17.25C21.2969 19.3125 18.25 21 14.5 21C10.7031 21 7.65625 19.3125 5.45312 17.25C3.25 15.1875 1.79688 12.75 1.09375 11.1094C0.953125 10.7344 0.953125 10.3125 1.09375 9.9375C1.79688 8.25 3.25 5.8125 5.45312 3.79688C7.65625 1.73438 10.7031 0 14.5 0C18.25 0 21.2969 1.73438 23.5 3.79688ZM14.5 3.75C10.75 3.75 7.75 6.79688 7.75 10.5C7.75 14.25 10.75 17.25 14.5 17.25C18.2031 17.25 21.25 14.25 21.25 10.5C21.25 6.79688 18.2031 3.75 14.5 3.75Z" fill="#6D3EFF" />
+          </svg>
+        </div>
+        <h2 class="text-[2.5rem] font-bold">
+          Visión
+        </h2>
+        <p class="text-xl text-balance">
+          <span class="font-bold">Nos visualizamos como líderes innovadores en la industria financiera, siendo reconocidos por nuestra capacidad para transformar la gestión de los flujos de rentas inmobiliarias y de cobranzas de flujos recurrentes en una experiencia sin preocupaciones.</span>
+          Aspiramos a ser la elección preferida de nuestros clientes, destacando por nuestra integridad, transparencia y el constante impulso hacia soluciones financieras vanguardistas. Buscamos ser pioneros en la creación de un camino hacia la estabilidad financiera, donde la confianza y la excelencia sean los pilares fundamentales de nuestro éxito.
+        </p>
+        <div>
+          <CommonBaseButton type="white">
+            <span>Quiero un adelanto</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M5 12H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M12 5L19 12L12 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </CommonBaseButton>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
