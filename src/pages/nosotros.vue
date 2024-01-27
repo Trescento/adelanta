dc401e4cbda10e9e42cb594c83cf4bc8dc50ff16
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-27 16:33:24 -0600

    Add NosotrosVisionSection component and update SEO meta

--- src/pages/nosotros.vue
+++ src/pages/nosotros.vue
@@ -1,4 +1,12 @@
 <template>
   <NosotrosQuienesSomosSection />
   <NosotrosMisionSection />
+  <NosotrosVisionSection />
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Nosotros',
+  description: 'Somos una empresa de desarrollo de software'
+})
+</script>
