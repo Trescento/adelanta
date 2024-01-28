c43e5303ad830bbf9add3de91a72d8d0ded3b973
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-28 00:17:54 -0600

    Add 'black' type to BaseButton component

--- src/components/common/BaseButton.vue
+++ src/components/common/BaseButton.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 const props = defineProps<{
-  type?: 'primary' | 'secondary' | 'ghost' | 'white'
+  type?: 'primary' | 'secondary' | 'ghost' | 'white' | 'black'
 }>()
 
 const baseClass = 'text-lg font-semibold px-6 py-4 rounded-lg flex items-center space-x-4 border'
@@ -15,7 +15,8 @@ const classes = {
   primary: 'text-white bg-brand border-brand',
   secondary: 'border-brand text-brand',
   ghost: 'border-transparent text-brand',
-  white: 'border-white text-white'
+  white: 'border-white text-white',
+  black: 'border-transparent text-black'
 }
 const customClass = classes[props.type!] ?? classes.primary
 const buttonClass = `${baseClass} ${customClass}`
