2d2930a90b039ccdc21d5e9297d25cc931a79ad0
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-30 01:53:02 -0600

    Add SocialIcon component

---
+++ src/components/common/SocialIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <img
+    :src="`/svgs/${iconMap[props.icon]}`"
+    :width="props.width"
+    :height="props.height"
+    :alt="`${props.icon} icon`"
+  >
+</template>
+
+<script setup lang="ts">
+
+type SocialIcon = 'linkedin' | 'facebook' | 'instagram' | 'twitter' | 'tiktok'
+
+const props = defineProps<{
+  icon: SocialIcon
+  width: number
+  height: number
+}>()
+
+const iconMap = {
+  linkedin: '/linkedin.svg',
+  facebook: '/facebook.svg',
+  instagram: '/instagram.svg',
+  twitter: '/x.svg',
+  tiktok: '/tiktok.svg'
+}
+
+</script>
