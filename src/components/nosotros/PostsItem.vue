0f46f034d5c7d4b876f7b6f7bdbbdeb0a4a49715
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-01-30 01:53:15 -0600

    Update button type in PostsItem.vue

--- src/components/nosotros/PostsItem.vue
+++ src/components/nosotros/PostsItem.vue
@@ -7,7 +7,7 @@
       {{ props.description }}
     </p>
     <div class="mt-auto">
-      <CommonBaseButton type="black">
+      <CommonBaseButton type="black-ghost">
         <span>Descubre más</span>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path d="M5 12H19" stroke="#150D26" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
