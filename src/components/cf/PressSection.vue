53635534544b5fa703133706a840a92a92dcf463
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-01 13:12:11 -0600

    Add PressSection component and update ViewComoFunciona.vue

---
+++ src/components/cf/PressSection.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="container flex flex-col my-24">
+    <div class="flex flex-col lg:flex-row gap-16">
+      <div class="flex flex-col">
+        <h2 class="text-[2.5rem] font-bold text-balance">
+          ¿Cómo solicito mi adelanto?
+        </h2>
+        <p class="text-balance">
+          Cualquier tipo de inmueble, sólo necesitas tu contrato de arrendamiento y registrarte en Adelanta por Ziff.
+        </p>
+      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-6">
+        <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 lg:h-72">
+          <div class="w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
+            1
+          </div>
+          <p class="text-brand">
+            Registrate y obtén tu pre-aprobación de inmediato
+          </p>
+        </div>
+        <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 lg:h-72">
+          <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
+            2
+          </div>
+          <p class="text-brand">
+            Registrate y obtén tu pre-aprobación de inmediato
+          </p>
+        </div>
+        <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 lg:h-72">
+          <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
+            3
+          </div>
+          <p class="text-brand">
+            Registrate y obtén tu pre-aprobación de inmediato
+          </p>
+        </div>
+        <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 lg:h-72">
+          <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
+            4
+          </div>
+          <p class="text-brand">
+            Registrate y obtén tu pre-aprobación de inmediato
+          </p>
+        </div>
+        <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 lg:h-72">
+          <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
+            5
+          </div>
+          <p class="text-brand">
+            Registrate y obtén tu pre-aprobación de inmediato
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flex items-center lg:justify-center  mt-6 lg:mt-12">
+      <CommonBaseButton>
+        <span class="font-normal">Registrarme</span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+          <path d="M5.5 12H19.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M12.5 5L19.5 12L12.5 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </CommonBaseButton>
+    </div>
+  </section>
+</template>
--- src/components/view/ViewComoFunciona.vue
+++ src/components/view/ViewComoFunciona.vue
@@ -1,56 +1,13 @@
 <template>
   <CfTitleSection />
   <CfHowSection />
-  <section class="container my-24 flex gap-16">
-    <div class="flex flex-col">
-      <h2 class="text-[2.5rem] font-bold text-balance">
-        ¿Cómo solicito mi adelanto?
-      </h2>
-      <p class="text-balance">
-        Cualquier tipo de inmueble, sólo necesitas tu contrato de arrendamiento y registrarte en Adelanta por Ziff.
-      </p>
+  <CfPressSection />
+  <section class="my-24 flex flex-col gap-10">
+    <div>
+      <HeroTextCarousel />
     </div>
-    <div class="grid grid-cols-3 gap-6">
-      <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 h-72">
-        <div class="w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
-          1
-        </div>
-        <p class="text-brand">
-          Registrate y obtén tu pre-aprobación de inmediato
-        </p>
-      </div>
-      <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 h-72">
-        <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
-          2
-        </div>
-        <p class="text-brand">
-          Registrate y obtén tu pre-aprobación de inmediato
-        </p>
-      </div>
-      <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 h-72">
-        <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
-          3
-        </div>
-        <p class="text-brand">
-          Registrate y obtén tu pre-aprobación de inmediato
-        </p>
-      </div>
-      <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 h-72">
-        <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
-          4
-        </div>
-        <p class="text-brand">
-          Registrate y obtén tu pre-aprobación de inmediato
-        </p>
-      </div>
-      <div class="bg-brand-purple-light rounded-3xl text-xl font-bold p-8 h-72">
-        <div class=" w-11 h-11 rounded-full bg-white items-center justify-center flex mb-6">
-          5
-        </div>
-        <p class="text-brand">
-          Registrate y obtén tu pre-aprobación de inmediato
-        </p>
-      </div>
+    <div>
+      <HeroImagesCarousel />
     </div>
   </section>
 </template>
