92149a4488899766f15abc1ff331e1be5695eb51
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-01 14:06:22 -0600

    Add ArrowIcon component

---
+++ src/components/common/ArrowIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="25"
+    viewBox="0 0 24 25"
+    fill="none"
+    :class="className"
+  >
+    <path d="M5 12.5H19" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M12 5.5L19 12.5L12 19.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+type arrowType = 'white' | 'black' | 'primary'
+const props = defineProps<{
+  type?: arrowType
+}>()
+
+const type = props.type ?? 'white'
+const classes = {
+  white: 'stroke-white',
+  black: 'stroke-black',
+  primary: 'stroke-brand'
+}
+
+const className = classes[type]
+
+</script>
