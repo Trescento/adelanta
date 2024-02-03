8c5e8e3eefef690038a9e2da5cf1bbd6bd191537
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-02 23:59:57 -0600

    Add section for reactivating customer portfolio

--- src/components/view/ViewSocios.vue
+++ src/components/view/ViewSocios.vue
@@ -1,4 +1,9 @@
 <template>
   <SociosHeroSection />
   <SociosHowSection />
+  <section class="my-24 container">
+    <h2 class="max-w-2xl text-[2.5rem] text-center font-bold text-balance mx-auto">
+      La manera más fácil de reactivar tu cartera de clientes
+    </h2>
+  </section>
 </template>
