54edd975b721eda862bed59fd79173b180eb7a3b
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-04 19:07:31 -0600

    Add RefiereAhoraSection component

---
+++ src/components/socios/RefiereAhoraSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class=" bg-brand-purple-secondary my-24 py-24">
+    <div class="flex flex-col gap-12">
+      <h3 class="text-white text-center text-[2rem] font-bold">
+        Refiere ahora y ¡comienza a ganar comisiones!
+      </h3>
+      <ImageInfo img="/how-nuestros-productos.jpeg" class="md:container" img-left>
+        <p class="font-bold text-[1.75rem] max-w-3xl text-balance">
+          Hemos adelantado más de $100 millones de pesos en ingresos a lo largo de la República Mexicana
+        </p>
+      </ImageInfo>
+      <div class="flex justify-center">
+        <CommonBaseButton type="white">
+          <span>Comenzar a referir</span>
+          <CommonArrowIcon type="white" />
+        </CommonBaseButton>
+      </div>
+    </div>
+  </section>
+</template>
