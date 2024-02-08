c72f2249ee2fff0bfeea6044a7ad43940a66fa6b
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-07 18:34:52 -0600

    Refactor ViewBlog component to use BlogHeroSection

---
+++ src/components/blog/HeroSection.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="bg-brand-gray90">
+    <div class="container py-24 flex flex-col lg:flex-row gap-16 lg:items-center">
+      <div class="flex flex-col gap-6 w-full max-w-[400px]">
+        <div class="flex items-center gap-2">
+          <div class=" bg-brand-purple-light rounded-full w-11 h-11 grid place-content-center">
+            <svg width="15" height="24" viewBox="0 0 15 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7.33203 0C8.12891 0 8.83203 0.703125 8.83203 1.5V3.1875C8.87891 3.1875 8.97266 3.23438 9.01953 3.23438C9.53516 3.28125 10.9883 3.5625 11.5977 3.70312C12.3945 3.89062 12.9102 4.73438 12.7227 5.53125C12.4883 6.32812 11.6914 6.79688 10.8945 6.60938C10.4258 6.46875 9.11328 6.28125 8.59766 6.1875C7.09766 5.95312 5.78516 6.09375 4.89453 6.42188C4.05078 6.75 3.67578 7.21875 3.58203 7.73438C3.48828 8.25 3.58203 8.53125 3.67578 8.71875C3.76953 8.90625 3.91016 9.09375 4.28516 9.32812C5.03516 9.84375 6.20703 10.1719 7.70703 10.5938H7.84766C9.16016 10.9688 10.8008 11.4375 12.0195 12.2344C12.6758 12.6562 13.2852 13.2656 13.6602 14.0625C14.082 14.9062 14.1289 15.8438 13.9883 16.8281C13.6602 18.6094 12.3945 19.8281 10.8945 20.4375C10.2383 20.7188 9.53516 20.8594 8.83203 20.9531V22.5C8.83203 23.3438 8.12891 24 7.33203 24C6.48828 24 5.83203 23.3438 5.83203 22.5V20.9062C5.78516 20.9062 5.78516 20.8594 5.73828 20.8594C4.61328 20.7188 2.73828 20.2031 1.47266 19.6406C0.675781 19.3125 0.347656 18.4219 0.675781 17.6719C1.00391 16.9219 1.89453 16.5469 2.64453 16.9219C3.62891 17.3438 5.26953 17.7656 6.20703 17.9062C7.70703 18.1406 8.92578 18 9.76953 17.6719C10.5664 17.3438 10.8945 16.875 11.0352 16.3125C11.1289 15.7969 11.0352 15.5156 10.9414 15.3281C10.8477 15.1406 10.707 14.9531 10.332 14.7188C9.58203 14.2031 8.41016 13.875 6.91016 13.4531H6.76953C5.45703 13.0781 3.81641 12.6094 2.59766 11.8125C1.94141 11.3906 1.33203 10.7812 0.957031 9.98438C0.535156 9.14062 0.488281 8.20312 0.628906 7.17188C1.00391 5.39062 2.31641 4.21875 3.81641 3.60938C4.42578 3.375 5.12891 3.23438 5.83203 3.14062V1.5C5.83203 0.703125 6.48828 0 7.33203 0Z" fill="#6D3EFF" />
+            </svg>
+          </div>
+          <div class=" bg-brand-purple-light rounded-full w-11 h-11 grid place-content-center">
+            <svg width="31" height="24" viewBox="0 0 31 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20.5 8.25C20.5 12.8438 16.0938 16.5 10.75 16.5C8.92188 16.5 7.28125 16.125 5.82812 15.375C4.65625 15.9844 3.15625 16.5 1.32812 16.5C1.1875 16.5 1.04688 16.4531 1 16.3125C0.953125 16.1719 0.953125 16.0312 1.09375 15.9375C1.09375 15.8906 2.17188 14.7656 2.92188 13.2188C1.70312 11.8125 1 10.125 1 8.25C1 3.70312 5.35938 0 10.75 0C16.0938 0 20.5 3.70312 20.5 8.25ZM13.6094 10.2656C13.9844 8.20312 12.0156 7.6875 10.9844 7.35938L10.7031 7.3125C9.625 6.98438 9.625 6.79688 9.67188 6.60938C9.71875 6.23438 10.4688 6.14062 11.0781 6.23438C11.3594 6.28125 11.6406 6.375 11.9219 6.46875C12.3906 6.65625 12.9531 6.375 13.0938 5.90625C13.2812 5.39062 13.0469 4.875 12.5312 4.6875C12.2031 4.59375 11.9219 4.5 11.6875 4.45312V3.9375C11.6875 3.42188 11.2656 3 10.75 3C10.1875 3 9.8125 3.42188 9.8125 3.9375V4.40625C8.73438 4.64062 7.98438 5.29688 7.84375 6.28125C7.46875 8.29688 9.39062 8.85938 10.1875 9.09375L10.4688 9.1875C11.8281 9.5625 11.8281 9.70312 11.7812 9.9375C11.7344 10.3125 10.9844 10.4062 10.375 10.3125C10.0469 10.2656 9.625 10.0781 9.25 9.98438L9.0625 9.89062C8.54688 9.70312 8.03125 9.98438 7.84375 10.4531C7.65625 10.9688 7.9375 11.4844 8.40625 11.6719L8.59375 11.7188C8.96875 11.8594 9.39062 12 9.8125 12.0938V12.5625C9.8125 13.125 10.1875 13.5 10.75 13.5C11.2656 13.5 11.6875 13.125 11.6875 12.5625V12.0938C12.7188 11.8594 13.4688 11.25 13.6094 10.2656ZM29.0781 20.8125C29.8281 22.3125 30.8594 23.3906 30.8594 23.4375C30.9531 23.5312 31 23.6719 30.9531 23.8125C30.9062 23.9531 30.7656 24 30.625 24C28.9375 24 27.4844 23.5312 26.3125 23.0156C25.0469 23.625 23.5469 24 22 24C17.7344 24 14.2188 21.375 13.2344 17.7656C18.25 16.7812 22 12.8906 22 8.25C22 8.01562 21.9531 7.78125 21.9531 7.54688C21.9531 7.54688 21.9531 7.5 22 7.5C26.9688 7.5 31 11.2031 31 15.75C31 17.6719 30.25 19.4062 29.0781 20.8125Z" fill="#6D3EFF" />
+            </svg>
+          </div>
+          <div class=" bg-brand-purple-light rounded-full w-11 h-11 grid place-content-center">
+            <svg width="27" height="24" viewBox="0 0 27 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M18.75 4.5L18.7031 4.54688C18.4688 4.54688 18.2344 4.5 18 4.5H12C11.2031 4.5 10.4531 4.64062 9.75 4.78125C9.75 4.6875 9.75 4.64062 9.75 4.5C9.75 2.01562 11.7188 0 14.25 0C16.7344 0 18.75 2.01562 18.75 4.5ZM18 6C18.1406 6 18.2812 6.04688 18.4688 6.04688C18.6562 6.04688 18.8438 6.04688 19.0781 6.09375C19.875 5.15625 21.0938 4.5 22.5 4.5H24L23.1094 8.0625C23.8594 8.71875 24.4219 9.5625 24.8438 10.5H25.5C26.2969 10.5 27 11.2031 27 12V16.5C27 17.3438 26.2969 18 25.5 18H24C23.5312 18.6094 23.0625 19.0781 22.5 19.5V22.5C22.5 23.3438 21.7969 24 21 24H19.5C18.6562 24 18 23.3438 18 22.5V21H12V22.5C12 23.3438 11.2969 24 10.5 24H9C8.15625 24 7.5 23.3438 7.5 22.5V19.5C5.85938 18.2812 4.73438 16.4062 4.5 14.25H3.1875C1.40625 14.25 0 12.8438 0 11.0625C0 9.32812 1.40625 7.875 3.1875 7.875H3.375C3.98438 7.875 4.5 8.39062 4.5 9C4.5 9.65625 3.98438 10.125 3.375 10.125H3.1875C2.625 10.125 2.25 10.5469 2.25 11.0625C2.25 11.5781 2.625 12 3.1875 12H4.64062C5.20312 9.23438 7.3125 6.98438 10.0781 6.28125C10.6875 6.09375 11.2969 6 12 6H18ZM19.875 11.25C19.2188 11.25 18.75 11.7656 18.75 12.375C18.75 13.0312 19.2188 13.5 19.875 13.5C20.4844 13.5 21 13.0312 21 12.375C21 11.7656 20.4844 11.25 19.875 11.25Z" fill="#6D3EFF" />
+            </svg>
+          </div>
+        </div>
+        <h1 class="font-bold text-[2.5rem] text-brand">
+          Bienvenido al blog de Adelanta por Ziff
+        </h1>
+        <p class="text-lg">
+          Entérate de toda la información del sector inmobiliario en México.
+        </p>
+      </div>
+      <div class="grid auto-rows-fr grid-cols-1 gap-6">
+        <div class="rounded-3xl flex bg-white overflow-hidden">
+          <div class="w-1/2 relative">
+            <img src="https://placehold.it/400x400" alt="Blog Hero Image" class="absolute top-0 bottom-0 left-0 right-0 w-full h-full object-cover">
+          </div>
+          <div class="flex flex-col p-6 gap-6 w-1/2">
+            <p class="font-bold text-2xl">
+              Cómo revalorizar tu local comercial: 7 consejos clave
+            </p>
+            <p class=" line-clamp-3">
+              Revaloriza tu local comercial, revaloriza tu patrimonio Poseer un local comercial es una inversión significativa, y maximizar su valor es esencial para el éxito a largo plazo. Ya sea que estés buscando atraer a inquilinos de alta calidad o aumentar
+            </p>
+            <CommonBaseButton type="ghost">
+              <span>Leer más</span>
+              <CommonArrowIcon type="primary" />
+            </CommonBaseButton>
+          </div>
+        </div>
+        <div class="rounded-3xl flex bg-white overflow-hidden">
+          <div class="w-1/2 relative">
+            <img src="https://placehold.it/400x400" alt="Blog Hero Image" class="absolute top-0 bottom-0 left-0 right-0 w-full h-full object-cover">
+          </div>
+          <div class="flex flex-col p-6 gap-6 flex-1">
+            <p class="font-bold text-2xl">
+              ¿Qué es el factoraje y como puede ayudarnos?
+            </p>
+            <p class=" line-clamp-3">
+              ¿Qué es el factoraje? ¿Existen mejores soluciones? El factoraje, también conocido como factoring, es una práctica financiera que ha ganado relevancia en el mundo empresarial como una herramienta estratégica para mejorar la liquidez y gestionar eficientemente el capital de trabajo.
+            </p>
+            <CommonBaseButton type="ghost">
+              <span>Leer más</span>
+              <CommonArrowIcon type="primary" />
+            </CommonBaseButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/components/view/ViewBlog.vue
+++ src/components/view/ViewBlog.vue
@@ -1,6 +1,3 @@
 <template>
-  <section>
-    <h1>Blog</h1>
-    <p>Blog content goes here</p>
-  </section>
+  <BlogHeroSection />
 </template>
