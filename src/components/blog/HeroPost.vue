10ebbc2a0f2a1fcd841992e480d9f8a7a19830a2
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-08 00:37:49 -0600

    Add HeroPost component and HeroPostProps interface

---
+++ src/components/blog/HeroPost.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="rounded-3xl flex flex-col lg:flex-row bg-white overflow-hidden">
+    <div class="w-full h-72 lg:w-1/2 lg:h-auto relative">
+      <img :src="post.image" alt="Blog Hero Image" class="absolute top-0 bottom-0 left-0 right-0 w-full h-full object-cover">
+    </div>
+    <div class="flex flex-col p-6 gap-6 lg:w-1/2">
+      <p class="font-bold text-2xl">
+        {{ post.title }}
+      </p>
+      <p class=" line-clamp-3">
+        {{ post.description }}
+      </p>
+      <CommonBaseButton type="ghost" as-anchor :href="post.slug">
+        <span class="font-medium">Leer más</span>
+        <CommonArrowIcon type="primary" />
+      </CommonBaseButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { type HeroPostProps } from '../../interfaces/HeroPost'
+defineProps<{
+  post: HeroPostProps
+}>()
+</script>
---
+++ src/interfaces/HeroPost.ts
@@ -0,0 +1,6 @@
+export interface HeroPostProps {
+  title: string
+  description: string
+  image: string
+  slug: string
+}
