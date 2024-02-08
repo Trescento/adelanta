85c879ad5a18828246181c9da7667c498e38287a
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-07 18:34:42 -0600

    Update blog page with SEO metadata and page meta

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,3 +1,13 @@
 <template>
   <ViewBlog />
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  headerColor: 'gray90'
+})
+useSeoMeta({
+  title: 'Blog | Adelanta por Ziff',
+  description: 'Entérate de toda la información del sector inmobiliario en México.'
+})
+</script>
