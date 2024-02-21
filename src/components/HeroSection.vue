23005c5dc1288876d200b75dec3e3f8ec0921957
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-02-21 14:24:56 -0600

    Update HeroSection.vue with new text and button styling

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -4,15 +4,20 @@
       <HeroImagesCarousel with-fade />
     </div>
     <div class="lg:mr-16 max-w-[700px] w-full flex flex-col lg:order-1">
-      <h1 class="text-4xl lg:text-[4rem] font-bold leading-[normal] text-balance mt-4 mb-6">
-        Accede a capital para hacer crecer tu negocio sin endeudarte.
-      </h1>
+      <div class="mt-4 mb-6">
+        <h1 class="text-4xl lg:text-[4rem] font-bold leading-[normal] text-balance">
+          Recibe por adelantado tus rentas sin endeudarte.
+        </h1>
+        <p class="font-bold px-1 text-lg lg:text-2xl">
+          Hasta 36 meses, en unos solos días
+        </p>
+      </div>
       <div>
         <HeroTextCarousel />
       </div>
       <div class="mt-6 lg:mt-20">
         <div class="flex">
-          <CommonBaseButton as-anchor href="https://app.arrenda.mx/register/" target="_blank">
+          <CommonBaseButton as-anchor href="https://app.arrenda.mx/register/" target="_blank" class="w-full flex justify-center lg:w-auto">
             <span class="block lg:hidden">Solicitar capital</span>
             <span class="hidden lg:block">Solicita capital por adelantado</span>
             <CommonArrowIcon />
